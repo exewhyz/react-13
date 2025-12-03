2ca8c5ee78092f95585eeb8a683f33eb1c789f3b
Aniket Raj <araj52444@gmail.com>
2025-12-03 12:57:04 +0530

    Form Handeling

---
+++ src/components/form.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(title)
+    setTitle("");
+  };
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <label htmlFor="title">Title</label>
+      <input
+        id="title"
+        type="text"
+        placeholder="Enter your Blog Title"
+        required
+        value={title}
+        onChange={(e)=>{ setTitle(e.target.value) }}
+      />
+      <button>Create Blog</button>
+    </form>
+  );
+}
